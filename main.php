1253cb6d405906e69c40d013e3b50040ab99f644
sibalnom <euljiro1@naver.com>
2015-07-07 21:30:36 +0900

    work main.php

--- main.php
+++ main.php
@@ -1,3 +1,20 @@
 <?php
-  echo '0.0.0.0:443';
+$postdata = file_get_contents("php://input");
+
+switch($postdata) {
+case "KR-Korea":
+    switch(mt_rand(1, 2)) {
+        case 1:
+            echo '1.247.12.132:443';
+            break;
+        case 2:
+            echo '162.243.157.40:443';
+            break;
+case "KR-Korea:teams":
+    echo 'jp-teams-1.koagario.tk:443';
+    break;
+case "KR-Korea:experimental":
+    echo 'jp-experimental-1.koagario.tk:443';
+    break;
+}
 ?>
